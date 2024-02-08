b7894fc9a72533368a7025330e159ce459010a8e
Sanglap-Halder <sanglaphalder0@gmail.com>
2024-02-09 02:01:00 +0530

    Added light and dark mode

--- src/header.js
+++ src/header.js
@@ -1,9 +1,23 @@
 import react, { useContext } from "react"
 import Profile from "./profile"
 import pic from "./logo1.png"
+import React from "react";
+import { ReactComponent as Sun } from "./Sun.svg";
+import { ReactComponent as Moon } from "./Moon.svg";
+import "./DarkMode.css";
 
 
 const Header=()=>{
+    const setDarkMode = () => {
+        document.querySelector("body").setAttribute('data-theme', 'dark');
+    };
+    const setLightMode = () => {
+        document.querySelector("body").setAttribute('data-theme', 'light');
+    };
+    const toggleTheme = (e) =>{
+        if(e.target.checked) setDarkMode();
+        else setLightMode();
+    };
     return(
         <header class="header" >
         <a href="#" class="logo">
@@ -32,6 +46,18 @@ const Header=()=>{
         <div class="search-form">
             <input type="text" class="search-input" id="search-box" placeholder="Search"/>
             <i class="fas fa-search"></i>
+        </div>
+        <div className='dark_mode'>
+            <input
+                className='dark_mode_input'
+                type='checkbox'
+                id='darkmode-toggle'
+                onChange={toggleTheme}
+            />
+            <label className='dark_mode_label' for='darkmode-toggle'>
+                <Sun />
+                <Moon />
+            </label>
         </div>
         
     </header>
