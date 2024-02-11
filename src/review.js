63c6cf4d5fe8f746dee850b913978230b35bad4b
Nepalivai <108126089+nepalivai@users.noreply.github.com>
2024-02-11 15:42:36 +0000

    removed unused links in footer.

--- src/review.js
+++ src/review.js
@@ -34,13 +34,6 @@ export default function Review(){
             <input type="text" class="search-input" placeholder="Search"/>
             <button class="btn btn-primary">search</button>
         </div>
-        <div class="share">
-            <a href="#" class="fab fa-facebook"></a>
-            <a href="#" class="fab fa-twitter"></a>
-            <a href="#" class="fab fa-instagram"></a>
-            <a href="#" class="fab fa-linkedin"></a>
-            <a href="#" class="fab fa-pinterest"></a>
-        </div>
         <div class="links">
             <a href="#home">home</a>
             <a href="#home">about</a>
