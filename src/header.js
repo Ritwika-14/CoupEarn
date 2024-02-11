4343fe692df3d78bedb3abfdfa1db90bf8bfbfd3
Nepalivai <108126089+nepalivai@users.noreply.github.com>
2024-02-11 15:18:23 +0000

    useless code clean from nav bar.

--- src/header.js
+++ src/header.js
@@ -32,21 +32,6 @@ const Header=()=>{
             
         </nav>
         <Profile />
-        <div class="buttons">
-            <button id="search-btn">
-                <i class="fas fa-search"></i>
-            </button>
-            <button id="cart-btn">
-                <i class="fas fa-shopping-cart"></i>
-            </button>
-            <button id="menu-btn">
-                <i class="fas fa-bars"></i>
-            </button>
-        </div>
-        <div class="search-form">
-            <input type="text" class="search-input" id="search-box" placeholder="Search"/>
-            <i class="fas fa-search"></i>
-        </div>
         <div className='dark_mode'>
             <input
                 className='dark_mode_input'
